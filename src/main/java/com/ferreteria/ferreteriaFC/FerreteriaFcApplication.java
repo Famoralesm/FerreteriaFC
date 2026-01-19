261320456eba5091ad1b3c89b80b4f0cb136e111
Felipe Morales <fm.moralespro@gmail.com>
2026-01-18 23:36:55 -0300

    Initial Spring Boot project - FerreteriaFC

---
+++ src/main/java/com/ferreteria/ferreteriaFC/FerreteriaFcApplication.java
@@ -0,0 +1,13 @@
+package com.ferreteria.ferreteriaFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaFcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaFcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferreteria/ferreteriaFC/FerreteriaFcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreteria.ferreteriaFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaFcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
